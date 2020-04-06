c2f6cd5d64b6d73f2980dcf5f8cfc878f3b42f89
cmcgahan <chmcgahan@gmail.com>
2020-04-06 16:05:45 +0200

    megaphone, my first program in c++

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+static char	*ft_lowtocap(char *str)
+{
+	int i = 0;
+	
+	while (str[i])
+	{	
+		if (97 <= str[i] && str[i] <= 122)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
+
+int		main(int argc, char **argv)
+{
+	int i = 1;
+
+	if (argc > 1)
+	{
+		while (i < argc)
+		{
+			std::cout << ft_lowtocap(argv[i]);
+			i++;
+		}
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	return (0);
+}
