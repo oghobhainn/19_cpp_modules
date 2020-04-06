5ff251bed1545ec8df9260eae44ed4c9e1fc9276
cmcgahan <chmcgahan@gmail.com>
2020-04-06 16:18:52 +0200

    first program in cpp

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -21,11 +21,12 @@ int		main(int argc, char **argv)
 	{
 		while (i < argc)
 		{
-			std::cout << ft_lowtocap(argv[i]) << std::endl;
+			std::cout << ft_lowtocap(argv[i]);
 			i++;
 		}
 	}
 	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	std::cout << std::endl;
 	return (0);
 }
