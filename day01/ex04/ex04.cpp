9394d7b10851929c8dfd6939c4e35269cabf6a38
Charlie Mcgahan <cmcgahan@fu-r6-p1.s19.be>
2021-04-29 10:13:38 +0200

    ex04

---
+++ day01/ex04/ex04.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *ptr;
+	ptr = &str;
+
+	std::string &ref = str;
+
+	std::cout << *ptr << std::endl;
+	std::cout << ref << std::endl;
+}
